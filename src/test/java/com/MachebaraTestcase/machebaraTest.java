f53aa6116660bdd7ad6362ab7f88bc83ab496a9c
Kimberly Ornopia <34766577+FDCkimTester@users.noreply.github.com>
2020-02-18 13:15:00 +0800

    Add testcases join, start bcast and send gift

--- src/test/java/com/MachebaraTestcase/machebaraTest.java
+++ src/test/java/com/MachebaraTestcase/machebaraTest.java
@@ -1,5 +1,6 @@
 package com.MachebaraTestcase;
 
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -33,13 +34,78 @@ public class machebaraTest {
     }
 
     @Test
-    public void login(){
+    public void loginViewer() throws InterruptedException {
         driver.get("http://variety.stg.inn.inmgt.com/");
         driver.findElement(By.id("js-login_modal")).click();
+        Thread.sleep(5000);
         driver.findElement(By.xpath("//*[@id='js-sns-login_modal_body']/div/div/div[3]/p[1]/span[1]")).click();
-        driver.findElement(By.id("user_id_or_mail")).sendKeys("kimmyornopia@gmail.com");
-        driver.findElement(By.name("password")).sendKeys("admin123");
-        driver.findElement(By.cssSelector("c-btn c-btn-color--blue c-btn--registerFrom")).click();
+        driver.findElement(By.id("user_id_or_mail")).sendKeys("kimmyornopia18@gmail.com");
+        driver.findElement(By.xpath("(//input[@class='c-form-inputText'])[5]")).sendKeys("admin123");
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    }
+
+    @Test
+    public void loginTalent() throws InterruptedException {
+        driver.get("http://variety.stg.inn.inmgt.com/");
+        driver.findElement(By.id("js-login_modal")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='js-sns-login_modal_body']/div/div/div[3]/p[1]/span[1]")).click();
+        driver.findElement(By.id("user_id_or_mail")).sendKeys("kimkim");
+        driver.findElement(By.xpath("(//input[@class='c-form-inputText'])[5]")).sendKeys("admin123");
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    }
+
+    @Test
+    public void registerViaEmail() throws InterruptedException {
+        String email = "kimoy"+ DateTime.now().toString("mmddYYYY")+"@mail.com";
+        driver.get("http://variety.stg.inn.inmgt.com/");
+        driver.findElement(By.id("js-login_modal")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='js-sns-login_modal_body']//span[@onclick='modal_jump_register()']")).click();
+        driver.findElement(By.id("register_mail_address")).sendKeys(email);
+        driver.findElement(By.xpath("(//input[@type='password'])[1]")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@id='register_nick_name']")).sendKeys("fdckim");
+        driver.findElement(By.xpath("(//button[@type='submit'])[1]//span")).click();
+    }
+
+    @Test
+    public void startBcast() throws InterruptedException {
+        loginTalent();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='btn-streaming-container']//span[contains(text(),'配信開始')]")).click();
+//        Thread.sleep(5000);
+//        driver.findElement(By.xpath("//*[@id='l-layer--modal']//button[contains(text(),'このお知らせを閉じる')]")).isDisplayed();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//button[contains(text(),'イベントの選択')]")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='l-layer--modal']//button[@data-event_id='419']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//button[@class='c-btn c-btn--cameraStart']//span[contains(text(),'配信スタート')]")).click();
+        Thread.sleep(7000);
+//        driver.findElement(By.xpath("//*[@id='js-room_start_form']//button[@class='c-btn c-btn--cameraStart']")).click();
+//        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='push-notif-yes']")).click();
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void joinBcast() throws InterruptedException {
+        loginViewer();
+        Thread.sleep(5000);
+        driver.get("http://variety.stg.inn.inmgt.com/talent/kim/room");
+    }
+
+    @Test
+    public void sendGift() throws InterruptedException {
+        joinBcast();
+        Thread.sleep(5000);
+        driver.get("http://variety.stg.inn.inmgt.com/talent/kim/room");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//i[@class='size-50 g-icon-gifting_id_33']")).click();
+        driver.findElement(By.xpath("//button[@class='js-giftSend js-giftSend_33 giftBtn c-planeBtn c-planeBtn-color--eGreen']")).click();
 
     }
+
+
+
 }
